758b86dea2810a5110eecdf62c262cae48bec1d4
allentsangdev <allentsang04@gmail.com>
2023-03-19 22:26:56 -0400

    update db connection

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ let dbConnection
 module.exports = {
 
     connectToDb: (cb) => {
-        MongoClient.connect(uri)
+        MongoClient.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
             .then((client) => {
                 dbConnection = client.db()
                 return cb()
